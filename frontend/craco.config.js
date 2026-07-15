80fac6f019a87a20c10f9347355428939dfc0505
Country of Christ <countryofchrist@gmail.com>
2026-07-15 12:28:27 -0400

    Modify webpack config to remove case sensitivity plugin
    Remove CaseSensitivePathsPlugin from webpack configuration.

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -5,6 +5,13 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    configure: (webpackConfig) => {
+      // Levels the playing field by removing the rigid case-checking plugin
+      webpackConfig.plugins = webpackConfig.plugins.filter(
+        (plugin) => plugin.constructor.name !== 'CaseSensitivePathsPlugin'
+      );
+      return webpackConfig;
+    }
   },
   style: {
     postcss: {

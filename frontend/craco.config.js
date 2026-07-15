ab9aa375fb49d4961efb6f59be30fc828d3aa4fe
Country of Christ <countryofchrist@gmail.com>
2026-07-15 11:27:04 -0400

    Remove health check plugin from craco config
    Removed health check plugin configuration from webpack.

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,31 +1,7 @@
-    // Add health check plugin to webpack if enabled
-    if (config.enableHealthCheck && healthPluginInstance) {
-      webpackConfig.plugins.push(healthPluginInstance);
-    }
-    return webpackConfig;
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [],
+    },
   },
 };
-
-webpackConfig.devServer = (devServerConfig) => {
-  // Add health check endpoints if enabled
-  if (config.enableHealthCheck && healthPluginInstance) {
-    const originalSetupMiddlewares = devServerConfig.setupMiddlewares;
-
-    devServerConfig.setupMiddlewares = (middlewares, devServer) => {
-      // Call original setup if exists
-      if (originalSetupMiddlewares) {
-        middlewares = originalSetupMiddlewares(middlewares, devServer);
-      }
-
-      // Setup health endpoints
-      setupHealthEndpoints(devServer, healthPluginInstance);
-
-      return middlewares;
-    };
-  }
-
-  return devServerConfig;
-};
-
-module.exports = webpackConfig;
-

cc46bb940d4309edfb8f2f5afa88f1fbe4a064ae
Country of Christ <countryofchrist@gmail.com>
2026-07-16 20:59:15 -0400

    Fix route elements and component imports in App.js
    Updated route elements for AdvertiserDashboard and Closet components.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import AudioPlayer from './components/AudioPlayer';
-import AdvertiserDashboard from "./components/AdvertiserDashboard";
+import AudioPlayer from "./components/AudioPlayer";
+import AdvertiserDashboard from "./>element={<AdvertiserDashboard/>}/>
 import Constitution from "./components/Constitution";
 import WitnessJury from "./components/WitnessJury";
 import DebtExileNotice from "./components/DebtExileNotice";
-import Closet from "./components/closet";
+import Closet from "./components/Closet";
+
 
 
 function App() {
   return (
     <div className="App">
         <Routes>
-          <Route path="/" element={<AudioPlayer />} />
+          
           <Route path="/merchants" element={<AdvertiserDashboard />} />
           <Route path="/constitution" element={<Constitution />} />
           <Route path="/witness-jury" element={<WitnessJury />} />
-          <Route path="/debt-exile" element={<DebtExileNotice />} />
+          <Route path="/debt-exile" element={<closet/>}/>
           <Route path="/closet" element={<closet />} />
         </Routes>
     </div>

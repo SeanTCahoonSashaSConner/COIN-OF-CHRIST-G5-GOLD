daec3e07bac515bece3e5c0f55d45620bc29b999
Country of Christ <countryofchrist@gmail.com>
2026-07-16 21:11:36 -0400

    Fix routes and import for AdvertiserDashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,22 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import AudioPlayer from "./components/AudioPlayer";
-import AdvertiserDashboard from "./>element={<AdvertiserDashboard/>}/>
+import AdvertiserDashboard from "./components/AdvertiserDashboard";
 import Constitution from "./components/Constitution";
 import WitnessJury from "./components/WitnessJury";
 import DebtExileNotice from "./components/DebtExileNotice";
 import Closet from "./components/Closet";
 
-
-
 function App() {
   return (
     <div className="App">
-        <Routes>
-          
-          <Route path="/merchants" element={<AdvertiserDashboard />} />
-          <Route path="/constitution" element={<Constitution />} />
-          <Route path="/witness-jury" element={<WitnessJury />} />
-          <Route path="/debt-exile" element={<closet/>}/>
-          <Route path="/closet" element={<closet />} />
-        </Routes>
+      <Routes>
+        <Route path="/merchants" element={<AdvertiserDashboard />} />
+        <Route path="/constitution" element={<Constitution />} />
+        <Route path="/witness-jury" element={<WitnessJury />} />
+        <Route path="/debt-exile" element={<DebtExileNotice />} />
+        <Route path="/closet" element={<Closet />} />
+      </Routes>
     </div>
   );
 }

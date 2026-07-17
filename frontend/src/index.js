15571c8abdee8382ce65bca3b0448b165e94bb48
Country of Christ <countryofchrist@gmail.com>
2026-07-16 21:22:12 -0400

    Remove basename from BrowserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/COIN-OF-CHRIST-G5-GOLD">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
+);

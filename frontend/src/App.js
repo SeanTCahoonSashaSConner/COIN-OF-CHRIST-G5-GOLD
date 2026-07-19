7a8302aaca289e9f5b5c3423e9c61bf0bdfb1070
Country of Christ <countryofchrist@gmail.com>
2026-07-19 19:59:04 -0400

    Add BrowserRouter for routing in App component
    Wrap the application in BrowserRouter and set the basename for routing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+// 1. Add BrowserRouter to your imports
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import AudioPlayer from "./components/AudioPlayer";
 import AdvertiserDashboard from "./components/AdvertiserDashboard";
 import Constitution from "./components/Constitution";
@@ -10,16 +11,20 @@ import Closet from "./components/Closet";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<AudioPlayer />} />
-        <Route path="/merchants" element={<AdvertiserDashboard />} />
-        <Route path="/constitution" element={<Constitution />} />
-        <Route path="/witness-jury" element={<WitnessJury />} />
-        <Route path="/debt-exile" element={<DebtExileNotice />} />
-        <Route path="/closet" element={<Closet />} />
-      </Routes>
-    </div>
+    // 2. Wrap the app in BrowserRouter with the basename
+    // Replace 'COIN-OF-CHRIST-G5-GOLD' with your actual GitHub repo name!
+    <BrowserRouter basename="/COIN-OF-CHRIST-G5-GOLD">
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<AudioPlayer />} />
+          <Route path="/merchants" element={<AdvertiserDashboard />} />
+          <Route path="/constitution" element={<Constitution />} />
+          <Route path="/witness-jury" element={<WitnessJury />} />
+          <Route path="/debt-exile" element={<DebtExileNotice />} />
+          <Route path="/closet" element={<Closet />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

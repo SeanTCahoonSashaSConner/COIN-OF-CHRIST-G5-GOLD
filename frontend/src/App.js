19fbfb3b55837d95e138c7a089fae53ec0956921
Country of Christ <countryofchrist@gmail.com>
2026-07-20 13:22:33 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,36 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AudioPlayer from "./components/AudioPlayer";
-import AdvertiserDashboard from "./components/AdvertiserDashboard";
-import Constitution from "./components/Constitution";
-import WitnessJury from "./components/WitnessJury";
-import DebtExileNotice from "./components/DebtExileNotice";
-import Closet from "./components/Closet";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import AudioPlayer from './components/AudioPlayer';
+import AdvertiserDashboard from './components/AdvertiserDashboard';
+import Constitution from './components/Constitution';
+import WitnessJury from './components/WitnessJury';
+import DebtExileNotice from './components/DebtExileNotice';
+import Closet from './components/Closet';
 
 function App() {
   return (
-    <BrowserRouter basename="/COIN-OF-CHRIST-G5-GOLD">
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<AudioPlayer />} />
-          <Route path="/merchants" element={<AdvertiserDashboard />} />
-          <Route path="/constitution" element={<Constitution />} />
-          <Route path="/witness-jury" element={<WitnessJury />} />
-          <Route path="/debt-exile" element={<DebtExileNotice />} />
-          <Route path="/closet" element={<Closet />} />
-          {/* Fallback route to prevent blank screens on unmatched paths */}
-          <Route path="*" element={<AudioPlayer />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Routes>
+      {/* Work / Slider Page */}
+      <Route path="/" element={<AudioPlayer />} />
+      
+      {/* Merchants / Heat Maps */}
+      <Route path="/merchants" element={<AdvertiserDashboard />} />
+      
+      {/* Sovereign Constitution */}
+      <Route path="/constitution" element={<Constitution />} />
+      
+      {/* Witness Jury Protocol */}
+      <Route path="/witness-jury" element={<WitnessJury />} />
+      
+      {/* Debt Exile Notice */}
+      <Route path="/debt-exile" element={<DebtExileNotice />} />
+      
+      {/* The Closet / Store */}
+      <Route path="/closet" element={<Closet />} />
+      
+      {/* Fallback */}
+      <Route path="*" element={<AudioPlayer />} />
+    </Routes>
   );
 }
 
